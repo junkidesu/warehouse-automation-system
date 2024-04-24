4eff1cf6a60f401ceb63300f36ea4e1391ed4ba3
Anwar Sh. <alvaro.sh03@gmail.com>
2024-04-25 03:29:16 +0500

    Read port from .env

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,6 +10,7 @@ import Network.Wai.Logger (withStdoutLogger)
 import Servant
 import Servant.Auth.Server (defaultCookieSettings, defaultJWTSettings, generateKey)
 import Swagger
+import System.Environment (lookupEnv)
 
 api :: Proxy API
 api = Proxy
@@ -17,6 +18,10 @@ api = Proxy
 startApp :: IO ()
 startApp = do
     onMissingFile (loadFile defaultConfig) (putStrLn "Missing .env file")
+
+    mbPort <- lookupEnv "PORT"
+
+    let port = maybe 3001 read mbPort
 
     conns <- initializeConnectionPool
 
@@ -30,5 +35,5 @@ startApp = do
                     server conns jwts
 
     withStdoutLogger $ \aplogger -> do
-        let settings = setPort 3001 $ setLogger aplogger defaultSettings
+        let settings = setPort port $ setLogger aplogger defaultSettings
         runSettings settings app
